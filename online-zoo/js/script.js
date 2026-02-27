40a8e78ca4198aa4adb7a8d4df178d5a5d6c556b
tsiklaurii <tsiklaurinino321@gmail.com>
2026-02-27 21:00:08 +0400

    feat: implement first pop up
    add 'together we care, save and protect!' pop up

---
+++ online-zoo/js/script.js
@@ -0,0 +1,11 @@
+function openDonationPopup1() {
+    const popup = document.getElementById("donationPopup1");
+    popup.classList.add("active");
+    document.body.classList.add("no_scroll");
+}
+
+function closeDonationPopup1() {
+    const popup = document.getElementById("donationPopup1");
+    popup.classList.remove("active");
+    document.body.classList.remove("no_scroll");
+}
